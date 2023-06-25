ef1933c32b57d528b6eee6be1bee7a1894987014
kitadeshogo <tori3227@gmail.com>
2023-06-26 01:26:27 +0900

    letは宣言時に値を代入しなくてもよい

---
+++ src/Sec_2/2.2.4.ts
@@ -0,0 +1,13 @@
+// let greeting = "Hello, ";
+// greeting = greeting + "world!!";
+// console.log(greeting);
+
+// let greeting, target;
+// greeting = "Hello, ";
+// target = "world!!";
+// console.log(greeting + target)
+
+let greeting: string, target: string;
+greeting = "Hello, ";
+target = "world!!";
+console.log(greeting + target);
