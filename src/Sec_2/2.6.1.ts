c9d2c6c94e0ebd1c542fbe5d3f9ad12397e653d2
kitadeshogo <tori3227@gmail.com>
2023-06-27 01:48:14 +0900

    2.6.1: FizzBuzz

---
+++ src/Sec_2/2.6.1.ts
@@ -0,0 +1,18 @@
+// let i = 1;
+// let str = ""
+
+
+// while (i <= 100) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         str += " FizzBuzz";
+//     } else if (i % 5 === 0) {
+//         str += " Buzz";
+//     } else if (i % 3 === 0) {
+//         str += " Fizz";
+//     } else {
+//         str += ` ${i}`
+//     }
+//     i++
+// }
+
+// console.log(str);
