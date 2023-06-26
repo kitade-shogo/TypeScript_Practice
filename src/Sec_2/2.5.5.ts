6982033e2a31845727e7af29e0088bef998f7b23
kitadeshogo <tori3227@gmail.com>
2023-06-27 01:27:06 +0900

    2.5.5: while文

---
+++ src/Sec_2/2.5.5.ts
@@ -0,0 +1,23 @@
+// let sum = 0;
+// let i = 1;
+
+// while (true) {
+//     if (i > 100) {
+//         break;
+//     }
+//     sum += i;
+//     i++
+// }
+
+// console.log(sum);
+
+
+// let i = 1;
+
+// while (i <= 100) {
+//     i++
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
