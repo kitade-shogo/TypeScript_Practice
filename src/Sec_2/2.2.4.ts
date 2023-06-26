4168fd0e1b3b6137f8ae0cd0a8d5575927625272
kitadeshogo <tori3227@gmail.com>
2023-06-26 12:28:36 +0900

    2.2.4

--- src/Sec_2/2.2.4.ts
+++ src/Sec_2/2.2.4.ts
@@ -7,7 +7,7 @@
 // target = "world!!";
 // console.log(greeting + target)
 
-let greeting: string, target: string;
-greeting = "Hello, ";
-target = "world!!";
-console.log(greeting + target);
+// let greeting: string, target: string;
+// greeting = "Hello, ";
+// target = "world!!";
+// console.log(greeting + target);
