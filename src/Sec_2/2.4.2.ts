fba7d5621f4de63deef178baef805774621e3322
kitadeshogo <tori3227@gmail.com>
2023-06-26 22:12:36 +0900

    2.4.2: +単項演算子を文字列の123の前につけて+123とすると、number型に変換される。

---
+++ src/Sec_2/2.4.2.ts
@@ -0,0 +1,20 @@
+// const x = 123;
+// const minusx = -x
+// console.log(minusx);
+
+
+// const str: string = '123';
+// console.log(+str * 100);
+
+// let foo = 10;
+// foo++;
+// console.log(foo);
+// --foo;
+// console.log(foo);
+
+// let foo = 10;
+// console.log(foo);
+// console.log(++foo);
+// console.log(foo);
+// console.log(foo--);
+// console.log(foo)
