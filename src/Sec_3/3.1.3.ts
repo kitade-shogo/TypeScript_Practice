c86362c23a869a2ec2904d4f01db08df2bbd50ab
kitadeshogo <tori3227@gmail.com>
2023-06-28 01:08:04 +0900

    3.1.3: 文字列をプロパティ名として使用できる

---
+++ src/Sec_3/3.1.3.ts
@@ -0,0 +1,26 @@
+// const obj = {
+//     "foo": 123,
+//     "foo bar": -500,
+//     '↑↓': ""
+// };
+
+// console.log(obj.foo);
+// console.log(obj["foo bar"]);
+
+
+// const obj = {
+//     1: "one",
+//     2.05: "tho point o five",
+// };
+
+
+// console.log(obj["1"]);
+// console.log(obj["2.05"]);
+
+
+// const propName = "foo";
+// const obj = {
+//     [propName]: 123
+// };
+
+// console.log(obj.foo);
