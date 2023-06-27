734bb19e572386a491f7325d8a21f6e7789173c0
kitadeshogo <tori3227@gmail.com>
2023-06-28 00:59:23 +0900

    3.1.2

---
+++ src/Sec_3/3.1.2.ts
@@ -0,0 +1,16 @@
+// const obj = {
+//     foo: 555,
+//     bar: "文字列",
+// };
+
+// const user = {
+//     name: input ? input : "名無し",
+//     age: 20,
+// };
+
+
+// const name = input ? input : "名無し";
+// const user = {
+//     name,
+//     age: 20,
+// };
