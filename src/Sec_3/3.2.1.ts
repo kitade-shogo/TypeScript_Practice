7481d06e4d7a074c06f7a8363727f702bcea26d2
kitadeshogo <tori3227@gmail.com>
2023-06-28 16:47:36 +0900

    3.2.1

---
+++ src/Sec_3/3.2.1.ts
@@ -0,0 +1,13 @@
+// const obj: {
+//     foo: number;
+//     bar: string:
+// } = {
+//     foo: 123,
+//     bar: "Hello, world"
+// };
+
+// const obj: {
+//     "foo bar": number;
+// } = {
+//     "foo bar": 123
+// }
