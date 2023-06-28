833bcc8b94fa26d5d6cf2f547666cf3fd1e5181c
kitadeshogo <tori3227@gmail.com>
2023-06-28 17:14:46 +0900

    3.2.2: type文は既にある型に別名をつけるだけのもの。また、typeによる型名の作成は、その型名を使うより後でも良い

---
+++ src/Sec_3/3.2.3.ts
@@ -0,0 +1,18 @@
+
+// const obj: FooBarObj = {
+//     foo: 123,
+//     bar: "Hello, world"
+// };
+
+// type FooBarObj = {
+//   foo: number;
+//   bar: string;
+// };
+
+// type UserId = string;
+// const id: UserId = "uhyo";
+
+// type FooObj = { foo: number };
+// type MyOBj = FooObj;
+
+// const obj: MyOBj = { foo: 0 };
