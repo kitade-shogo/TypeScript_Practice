594db16fa6931a2d8b10a6384ce9f80c12eadb8a
kitadeshogo <tori3227@gmail.com>
2023-06-28 18:26:52 +0900

    3.2.8: typeofを使うことで変数宣言から型を取り出して使える

---
+++ src/Sec_3/3.2.8.ts
@@ -0,0 +1,14 @@
+// const num: number = 0;
+// type T = typeof num;
+// const foo: T = 123;
+
+// const obj = {
+//     foo: 123,
+//     bar: "hi"
+// }
+
+// type T = typeof obj;
+// const obj2: T = {
+//     foo: -50,
+//     bar: ""
+// };
