7b6ae87737b59cf98d29a8cb35d97567d38f295d
kitadeshogo <tori3227@gmail.com>
2023-06-28 16:55:09 +0900

    3.2.2: オブジェクト型の型チェック

---
+++ src/Sec_3/3.2.2.ts
@@ -0,0 +1,16 @@
+// const obj: {
+//     foo: number;
+//     bar: string;
+// } = {
+//     foo: 123,
+//     bar: true
+// }
+
+// const obj = {
+//     foo: 123,
+//     bar: "Hello, world"
+// };
+
+// // obj.foo = null;
+
+// console.log(obj.hoge)
