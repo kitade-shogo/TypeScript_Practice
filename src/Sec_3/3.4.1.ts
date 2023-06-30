c1542fd3d162afe08175df4dfce71208797dfc94
kitadeshogo <tori3227@gmail.com>
2023-06-30 12:27:33 +0900

    3.4.1: 型定義内で使用できる型を引数のように渡すことができる。このような型のことをジェネリクス型と言う

---
+++ src/Sec_3/3.4.1.ts
@@ -0,0 +1,11 @@
+// type User<T> = {
+//     name: string;
+//     child: T;
+// };
+
+// type Family<Parent, Child> = {
+//     mother: Parent;
+//     father: Parent;
+//     child: Child;
+// };
+
