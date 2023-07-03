435c0dd4a2bb892bacf1a36f5e61bfc19bc9bbbf
kitadeshogo <tori3227@gmail.com>
2023-07-03 23:56:27 +0900

    3.4.3

---
+++ src/Sec_3/3.4.3.ts
@@ -0,0 +1,19 @@
+// type HasName = {
+//     name: string;
+// };
+
+// type Family<Parent extends HasName, Child extends HasName> = {
+//     mother: Parent;
+//     father: Parent;
+//     child: Child;
+// };
+// type Animal = {
+//     name: string;
+// };
+
+// type Human = {
+//     name: string;
+//     age: number;
+// };
+
+// type T = Family<Animal, Human>;
