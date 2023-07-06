15941dec3008157522a55d186e9c61f42b17b701
kitadeshogo <tori3227@gmail.com>
2023-07-07 00:25:21 +0900

    3.5.3

--- src/Sec_3/3.5.2.ts
+++ src/Sec_3/3.5.2.ts
@@ -1,8 +1,8 @@
-const arr = [0, 123, -456 * 100];
-// console.log(arr[0]);
-// console.log(arr[1]);
+// const arr = [0, 123, -456 * 100];
+// // console.log(arr[0]);
+// // console.log(arr[1]);
 
-console.log(arr);
+// console.log(arr);
 
-arr[1] = 5400;
-console.log(arr);
+// arr[1] = 5400;
+// console.log(arr);
---
+++ src/Sec_3/3.5.3.ts
@@ -0,0 +1,15 @@
+// const arr: number[] = [1, 10, 100];
+
+// const arr2: string[] = [123, -456];
+
+// const arr1: boolean[] = [false, true];
+
+// const arr2: Array<{ name: string; }> = [
+//     { name: "山田さん" },
+//     { name: "田中さん" },
+//     { name: "鈴木さん" }
+// ];
+
+// const arr: number[] = [1, 10, 100];
+
+// const arr2: (string|number|boolean)[] = [100, "文字列", false];
