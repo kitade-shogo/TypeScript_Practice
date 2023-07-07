3546391f54137850ab296e6b0b2217d749887388
kitadeshogo <tori3227@gmail.com>
2023-07-07 23:48:54 +0900

    3.5.5: 配列のメソッド。includesで与えられた値が入っているかどうかをbooleanで返す

---
+++ src/Sec_3/3.5.5.ts
@@ -0,0 +1,11 @@
+// const arr = [1, 10, 100];
+// // const arr: readonly number[] = [1, 10, 100];
+// console.log(arr.length)
+// arr.push(1000);
+// console.log(arr.length)
+// console.log(arr);
+// console.log(arr.includes(100));
+// console.log(arr.includes(50));
+
+// arr.push("hoge"); 今回はnumber[]型なのでコンパイルエラー。型推論が行われている?
+
