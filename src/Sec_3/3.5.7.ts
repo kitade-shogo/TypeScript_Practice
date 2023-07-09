e8bc7763400cbf2eb5df522af2df677bda52d1b5
kitadeshogo <tori3227@gmail.com>
2023-07-09 23:46:46 +0900

    3.5.7: タプル型を使用することで、要素数が固定された配列をσ区政することができ、それぞれの要素に異なる型を与えることができる

---
+++ src/Sec_3/3.5.7.ts
@@ -0,0 +1,14 @@
+// let tuple: [string, number] = ["foo", 0];
+// tuple = ["aoueo", -555];
+
+// const str = tuple[0];
+// const num = tuple[1];
+
+// // const nothing = tuple[2];
+
+
+// type User = [name: string, age: number];
+
+// const uyho: User = ["uhyo", 26];
+
+// console.log(uyho[1]);
