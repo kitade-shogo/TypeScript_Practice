ecd70c8af02f9a94b59c7e0fb126edc40a9afe89
kitadeshogo <tori3227@gmail.com>
2023-07-15 00:53:37 +0900

    3.6.5: restパターンでオブジェクトの残りを取得する

---
+++ src/Sec_3/3.6.5.ts
@@ -0,0 +1,20 @@
+// const obj = {
+//     foo: 123,
+//     bar: "string",
+//     baz: false
+// };
+
+// const { foo, ...restObj } = obj;
+
+// console.log(foo);
+// console.log(restObj);
+
+
+// const arr = [1, 1, 2, 3, 5, 8, 13];
+
+// const [first, second, third, ...rest] = arr;
+// console.log(first)
+// console.log(second)
+// console.log(third)
+// console.log(rest)
+
