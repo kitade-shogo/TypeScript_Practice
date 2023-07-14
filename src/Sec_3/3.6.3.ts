cb7713102417ae206b00d2946ef0da6c6f7beac0
kitadeshogo <tori3227@gmail.com>
2023-07-14 22:36:05 +0900

    3.6.3: 配列の分割代入

---
+++ src/Sec_3/3.6.3.ts
@@ -0,0 +1,13 @@
+// const arr = [1, 2, 4, 8, 16, 32];
+
+// const [,,,first, second, third] = arr;
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// const tuple: [string, number] = ["uyho", 26];
+
+// const [myName, age] = tuple;
+
+// console.log(myName);
+// console.log(age);
