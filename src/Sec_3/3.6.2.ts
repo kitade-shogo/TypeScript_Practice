f90e1ff7c4da3023150bb2ed607f547f81d56557
kitadeshogo <tori3227@gmail.com>
2023-07-14 22:28:16 +0900

    3.6.2: ネストした分割代入

---
+++ src/Sec_3/3.6.2.ts
@@ -0,0 +1,11 @@
+// const nested = {
+//     num: 123,
+//     obj: {
+//         foo: "world",
+//         bar: "hello"
+//     }
+// }
+
+// const { num, obj: { foo } } = nested;
+// console.log(num);
+// console.log(foo);
