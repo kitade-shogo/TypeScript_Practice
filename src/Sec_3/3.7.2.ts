8fd2d84906bfae9957a97545dfe01f6a550f4a38
kitadeshogo <tori3227@gmail.com>
2023-07-17 23:54:56 +0900

    3.7.2: 正規表現

---
+++ src/Sec_3/3.7.2.ts
@@ -0,0 +1,12 @@
+// const r = /ab+c/
+
+// console.log(r.test("abbbbc"))
+// console.log(r.test("Helllo, abc world"))
+// console.log(r.test("ABC"))
+// console.log(r.test("こんにちは"))
+// console.log(r.test(""))
+
+// const r = /^ab+c/;
+
+// console.log(r.test("abbbbc"))
+// console.log(r.test("Helllo, abc world"))
