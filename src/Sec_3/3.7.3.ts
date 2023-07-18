f4c8c0ab803ca35255637202a30aa3dec70fc9e1
kitadeshogo <tori3227@gmail.com>
2023-07-18 23:10:57 +0900

    3.7.3: 正規表現・キャプチャリンググループ

---
+++ src/Sec_3/3.7.3.ts
@@ -0,0 +1,8 @@
+// // console.log("Hello, abbbbbbc world! abbc".replace(/ab+c/,"foobar"))
+// // console.log("Hello, abbbbbbc world! abbc".replace(/ab+c/g, "foobar"))
+// const result = "Hello, abbbbbbc world! abbc".match(/a(?<worldName>b+)c/);
+// if (result !== null) {
+//     // console.log(result[0])
+//     // console.log(result[1])
+//     console.log(result.groups)
+// }
