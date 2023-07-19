0ee53c630f854f3816ed0972de52ae5d62860805
kitadeshogo <tori3227@gmail.com>
2023-07-19 23:59:51 +0900

    3.8.3: 別解

---
+++ src/Sec_3/3.8.3.ts
@@ -0,0 +1,32 @@
+type User = {
+    name: string;
+    age: number;
+    premiumUser: boolean;
+}
+
+const data: string = `
+uyho,26,1
+John Smith,17,0
+Mary Sue,14,1
+`;
+
+const users: User[] = data.split("\n")
+    .filter(line => line !== "")
+    .map(line => {
+        const [name, ageString, premiumUserString] = line.split(",")
+        
+        return {
+            name,
+            age: Number(ageString),
+            premiumUser: premiumUserString === "1"
+
+        }
+})
+
+for (const user of users) {
+    if (user.premiumUser) {
+        console.log(`${user.name} (${user.age})はプレミアムユーザーです。`);
+    } else {
+        console.log(`${user.name} (${user.age})はプレミアムユーザーではありません。`);
+    }
+}
