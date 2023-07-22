729721a81dfa53c431307b83360764f902585280
kitadeshogo <tori3227@gmail.com>
2023-07-22 23:19:04 +0900

    4.1.3: 関数式で関数を作成する場合には巻き上げは起こらない

---
+++ src/Sec_4/4.1.3.ts
@@ -0,0 +1,11 @@
+// type Human = {
+//     height: number;
+//     weight: number;
+// };
+
+// const calcBMI = function (human: Human): number {
+//     return human.weight / human.height ** 2
+// };
+// const uyho: Human = { height: 1.84, weight: 72 };
+
+// console.log(calcBMI(uyho));
