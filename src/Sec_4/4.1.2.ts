2504bd085d51480a0ef50962202051cd000099d6
kitadeshogo <tori3227@gmail.com>
2023-07-22 23:03:08 +0900

    4.1.2: 返り値がない関数にはvoidを使う。あるいは早期return

---
+++ src/Sec_4/4.1.2.ts
@@ -0,0 +1,20 @@
+// function hellloWorldNTimes(n: number): void {
+//   for (let i = 0; i < n; i++) {
+//     console.log("Hello, world!");
+//   }
+// }
+
+// hellloWorldNTimes(5);
+
+// function hellloWorldNTimes2(n: number): void {
+//   if (n >= 100) {
+//     console.log(`${n}回なんて無理です！`);
+//     return;
+//   }
+//   for (let i = 0; i < n; i++) {
+//     console.log("Hello, world!");
+//   }
+// }
+
+// hellloWorldNTimes2(5);
+// hellloWorldNTimes2(150);
