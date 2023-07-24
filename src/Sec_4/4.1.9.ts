d73746f309b17b7e2ec23acd620d37051c50ad2d
kitadeshogo <tori3227@gmail.com>
2023-07-24 23:53:55 +0900

    4.1.9: オプショナル引数。rest引数と併用する場合はオプショナル引数を先に書く

---
+++ src/Sec_4/4.1.9.ts
@@ -0,0 +1,11 @@
+// const toLowerOrUpper = (str: string, upper: boolean = false): string => {
+//     if (upper) {
+//         return str.toLocaleUpperCase();
+//     } else {
+//         return str.toLocaleLowerCase();
+//     }
+// }
+
+// console.log(toLowerOrUpper("Hello"));
+// console.log(toLowerOrUpper("Hello", false));
+// console.log(toLowerOrUpper("Hello",true));
