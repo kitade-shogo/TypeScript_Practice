b0872e01a40f75f232ed3dcc2cbfa5833d2091f9
kitadeshogo <tori3227@gmail.com>
2023-07-24 22:40:08 +0900

    4.1.6: 関数をオブジェクトとして使用する

---
+++ src/Sec_4/4.1.6.ts
@@ -0,0 +1,9 @@
+// const onj = {
+//     double(num: number): number {
+//         return num * 2
+//     },
+//     double2: (num: number): number => num * 2
+// };
+
+// console.log(onj.double(100));
+// console.log(onj.double2(-50));
