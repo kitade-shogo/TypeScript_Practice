7d2e90b21b2b17935f6f6594f9c75700aeecf90d
kitadeshogo <tori3227@gmail.com>
2023-07-25 00:09:55 +0900

    4.1.10: コールバック関数。filterやmapは高階関数である

---
+++ src/Sec_4/4.1.10.ts
@@ -0,0 +1,21 @@
+// type User = { name: string, age: number };
+// const getName = (u: User): string => {
+//     console.log("u is", u);
+//     return u.name
+// };
+// const users: User[] = [
+//     { name: "uyho", age: 26 },
+//     { name: "John Smith", age: 15 }
+// ];
+
+// const names = users.map(getName);
+// console.log(names);
+
+// type User = { name: string; age: number };
+// const users: User[] = [
+//   { name: "uyho", age: 26 },
+//   { name: "John Smith", age: 15 },
+// ];
+
+// const names = users.map((u :User): string => u.name);
+// console.log(names);
