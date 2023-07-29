e8b7e89e3af9b5743b20a2acf6871c7b6796b193
kitadeshogo <tori3227@gmail.com>
2023-07-30 00:05:17 +0900

    4.2.5: コールシグネチャ

--- src/Sec_1/1.2/1.2.1.ts
+++ src/Sec_1/1.2/1.2.1.ts
@@ -1,10 +1,10 @@
-function double(value: string | number) {
-    if (typeof value === "number") {
-        console.log(value * 2);
-    } else {
-        console.log(value.repeat(2));
-    }
-}
+// function double(value: string | number) {
+//     if (typeof value === "number") {
+//         console.log(value * 2);
+//     } else {
+//         console.log(value.repeat(2));
+//     }
+// }
 
-double(123)
-double("hello")
+// double(123)
+// double("hello")
