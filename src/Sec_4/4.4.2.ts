ccf8bdaec16bf0ff365e489609c03ccb1de32e1d
kitadeshogo <tori3227@gmail.com>
2023-08-01 23:37:15 +0900

    4.4.2: 関数の型引数の宣言

---
+++ src/Sec_4/4.4.2.ts
@@ -0,0 +1,53 @@
+// // const repeat = function <T>(element: T, length: number): T[] {
+// //     const result: T[] = [];
+// //     for (let i = 0; i < length; i++){
+// //         result.push(element);
+// //     }
+// //     return result;
+// // }
+
+// // const repeat = <T>(element: T, length: number): T[] => {
+// //     const result: T[] = [];
+// //     for (let i = 0; i < length; i++){
+// //         result.push(element);
+// //     }
+// //     return result;
+// // }
+
+// // const utils = {
+// //     repeat<T>(element: T, length: number): T[] {
+// //         const result: T[] = [];
+// //         for (let i = 0; i < length; i++){
+// //             result.push(element);
+// //         }
+// //         return result;
+// //     }
+// // }
+
+
+// // const pair = <Left, Right>(left: Left, right: Right): [Left, Right] => [left, right];
+// // const p = pair<string, number>("uhyo", 26);
+
+// const repeat = <T extends {
+//     name: string;
+// }>(element: T, length: number): T[] => {
+//     const result: T[] = [];
+//     for (let i = 0; i < length; i++){
+//         result.push(element);
+//     }
+//     return result;
+// }
+
+// type HasNameAndAge = { 
+//     name: string;
+//     age: number;
+// }
+
+
+// console.log(repeat<HasNameAndAge>({
+//     name: "uyho",
+//     age: 26,
+// }, 3));
+
+// // console.log(repeat<string>("a", 5));
+
