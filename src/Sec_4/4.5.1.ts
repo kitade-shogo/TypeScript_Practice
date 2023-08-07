928ed913d383289085ed2ed7b2314344a8de7332
kitadeshogo <tori3227@gmail.com>
2023-08-07 22:41:02 +0900

    4.5.1: 変数のスコープ

---
+++ src/Sec_4/4.5.1.ts
@@ -0,0 +1,11 @@
+// const repeatLength = 5;
+
+// const repeat = function <T>(element: T, length: number): T[] {
+//     const result: T[] = [];
+//     for (let i = 0; i < repeatLength; i++) {
+//         result.push(element);
+//     }
+//     return result;
+// };
+
+// console.log(result);
