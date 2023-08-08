dfa7e81169a8ffd15f70c702850ec7dc51c97f40
kitadeshogo <tori3227@gmail.com>
2023-08-08 22:20:50 +0900

    4.6.1: 練習

---
+++ src/Sec_4/4.6.1.ts
@@ -0,0 +1,24 @@
+// const getFizzBuzzString = (i: number) => {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     return "FizzBuzz";
+//   } else if (i % 3 === 0) {
+//     return "Fizz";
+//   } else if (i % 5 === 0) {
+//     return "Buzz";
+//   } else {
+//     return i;
+//   }
+// };
+
+// const sequence = (i: number, j: number): number[] => {
+//     const arr: number[] = []
+//     for (let k = i; k <= j; k++) { 
+//         arr.push(k)
+//     }
+//     return arr;
+// }
+
+// for (const i of sequence(1, 100)) {
+//     const message = getFizzBuzzString(i);
+//     console.log(message);
+// }
