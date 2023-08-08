e8ca3cada10ff983baa895eee95c600a98b7144c
kitadeshogo <tori3227@gmail.com>
2023-08-08 22:43:31 +0900

    4.6.3: コールバック間巣の練習

---
+++ src/Sec_4/4.6.3.ts
@@ -0,0 +1,11 @@
+// function map<T,U>(array: T[], callback: (value: T) => U): U[] {
+//     const arr: U[] = [];
+//     for (const elm of array) {
+//         result.push(callback(elm))
+//     }
+//     return arr;
+// }
+
+// const data = [1, 1, 2, 3, 5, 8, 13];
+// const result = map<number,number>(data, (x) => x * 10);
+// console.log(result);
