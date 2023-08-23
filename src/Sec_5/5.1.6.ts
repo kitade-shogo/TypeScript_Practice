84027756889204d87dac999bd1e3724a26e4a295
kitadeshogo <tori3227@gmail.com>
2023-08-23 23:05:10 +0900

    privateメソッドはクラス内部でしか使用できない

---
+++ src/Sec_5/5.1.6.ts
@@ -0,0 +1,17 @@
+// class User { 
+//     name: string;
+//     private age: number;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     public isAdult(): boolean {
+//         return this.age >= 20;
+//     }
+// }
+
+// const uhyo = new User("uhyo", 26);
+// console.log(uhyo.name);
+// console.log(uhyo.age)
