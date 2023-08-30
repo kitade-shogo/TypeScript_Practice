48ec5a0075e800a0be4142a79d3b6a4e59e5de83
kitadeshogo <tori3227@gmail.com>
2023-08-30 21:54:21 +0900

    5.3.4: 安易にprotectedを使うと、小クラスからの干渉を受けて予想しない挙動を示す可能性がある

---
+++ src/Sec_5/5.3.4.ts
@@ -0,0 +1,28 @@
+// class User {
+//     name: string;
+//     protected age: number;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     public isAdult(): boolean {
+//         return this.age >= 20;
+//     }
+// }
+
+// class PremiumUser extends User { 
+//     public isAdult(): boolean {
+//         return this.age >= 10;
+//     }
+// }
+
+
+// const miniuhyo = new PremiumUser("uhyo", 15);
+// const john = new User("John Smith", 15);
+
+// console.log(miniuhyo.isAdult());
+// console.log(john.isAdult());
+
+// // console.log(miniuhyo.age)
