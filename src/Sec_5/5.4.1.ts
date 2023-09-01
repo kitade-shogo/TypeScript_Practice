4ab003ebf1776a34c498a12d6b22e295cf85d4f8
kitadeshogo <tori3227@gmail.com>
2023-09-01 23:04:30 +0900

    5.4.1: 関数内のthisは呼び出され方によって決まる

---
+++ src/Sec_5/5.4.1.ts
@@ -0,0 +1,18 @@
+// class User { 
+//     name: string;
+//     #age: number;
+
+//     constructor(name: string, age: number) { 
+//         this.name = name;
+//         this.#age = age;
+//     }
+
+//     public isAdult(): boolean {
+//         return this.#age >= 20;
+//     }
+// }
+
+// const uhyo = new User("uhyo", 26);
+
+// const isAdult = uhyo.isAdult;
+// console.log(isAdult());
